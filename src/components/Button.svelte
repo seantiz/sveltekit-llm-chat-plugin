71ff8460f70ac1b17d0102bf190f5fa4a4778381
sean <135666428+seantiz@users.noreply.github.com>
2025-12-18 14:38:06 +0000

    chore: Update dependencies

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -1,20 +1,20 @@
 <script lang="ts">
-	import type { MouseEventHandler, KeyboardEventHandler } from 'svelte/elements';
+	import type { MouseEventHandler, KeyboardEventHandler } from 'svelte/elements'
 
 	interface ButtonProps {
-		tag?: string;
-		href?: string;
-		disabled?: boolean;
-		onclick?: MouseEventHandler<HTMLButtonElement>;
-		onkeydown?: KeyboardEventHandler<HTMLButtonElement>;
-		onkeyup?: KeyboardEventHandler<HTMLButtonElement>;
-		label?: string;
-		class?: string;
-		type?: 'button' | 'reset' | 'submit';
-		children?: () => any;
+		tag?: string
+		href?: string
+		disabled?: boolean
+		onclick?: MouseEventHandler<HTMLButtonElement>
+		onkeydown?: KeyboardEventHandler<HTMLButtonElement>
+		onkeyup?: KeyboardEventHandler<HTMLButtonElement>
+		label?: string
+		class?: string
+		type?: 'button' | 'reset' | 'submit'
+		children?: () => any
 	}
 
-	const {
+	let {
 		tag = undefined,
 		href = undefined,
 		disabled = false,
@@ -25,22 +25,22 @@
 		class: className = '',
 		type = 'button',
 		...rest
-	}: ButtonProps = $props();
+	}: ButtonProps = $props()
 
-	const element = $derived(tag ?? (href ? 'a' : 'button'));
-	const role = $derived(!['button', 'a'].includes(element) ? 'button' : undefined);
+	const element = $derived(tag ?? (href ? 'a' : 'button'))
+	const role = $derived(!['button', 'a'].includes(element) ? 'button' : undefined)
 
 	const onClick: MouseEventHandler<HTMLButtonElement> = (e) => {
-		onclick?.(e);
-	};
+		onclick?.(e)
+	}
 
 	const onKeydownEnter: KeyboardEventHandler<HTMLButtonElement> = (e) => {
-		onkeydown?.(e);
-	};
+		onkeydown?.(e)
+	}
 
 	const onKeyUpEnter: KeyboardEventHandler<HTMLButtonElement> = (e) => {
-		onkeyup?.(e);
-	};
+		onkeyup?.(e)
+	}
 </script>
 
 <button
